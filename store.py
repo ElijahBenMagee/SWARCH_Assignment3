2a7ea25c76174af4c25b1d9efa18af8d719dff4c
ElijahBenMagee <48970303+ElijahBenMagee@users.noreply.github.com>
2020-10-18 15:47:11 -0500

    push1

--- store.py
+++ store.py
@@ -11,278 +11,291 @@ conn = sqlite3.connect('shop.db')
 cur = conn.cursor()
 
 #main page of the store
-def mainPage(currUserID):
+def mainPage(currUserID,user):
+        cart = Cart()
+        while True:
 
-	while True:
+                try:
+                        print("\n --------------- Store Home Page ---------------\n")
+                        print("\n What would you like to do?:\n 1. View Items to Purchase\n 2. Build Cart \n 3. View Your Previous Orders \n 4. Logout of the Store\n 5. Exit the Program")
 
-		try:
-			print("\n --------------- Store Home Page ---------------\n")
-			print("\n What would you like to do?:\n 1. View Items to Purchase\n 2. Build Cart \n 3. View Your Previous Orders \n 4. Logout of the Store\n 5. Exit the Program")
+                        #selection variable for user to select what they want to do in the store
+                        selection = int(input("\n Please select an option (1, 2, 3, 4, or 5): "))
 
-			#selection variable for user to select what they want to do in the store
-			selection = int(input("\n Please select an option (1, 2, 3, 4, or 5): "))
+                        #display inventory of store
+                        if (selection == 1):
+                                item=Item()
+                                item.displayItems()
+                        #add items to cart
+                        elif (selection == 2):
+                                cart.cartAdd(currUserID)
 
-			#display inventory of store
-			if (selection == 1):
-				displayItems()
-
-			#add items to cart
-			elif (selection == 2):
-				cartAdd(currUserID)
-
-			#display previous orders 
-			elif (selection == 3):
-				previousOrders(currUserID)
-
-
-			#logout of current account
-			elif (selection == 4):
-				print("\n --------------- Thanks you for shopping! ---------------\n")
-				loginSetup()
-
-			#exit the program
-			elif (selection == 5):
-				i = 1
-				print("\n --------------- Thanks you for shopping! ---------------\n")
-
-				#closing database connection and exiting program
-				conn.close()
-				exit()
-
-			else:
-				print("\n ***Please enter a valid option. (1, 2, 3, 4, or 5)***")
-
-		except Exception as ex:
-			print("\n ***Please enter a vaild option. (1, 2, 3, 4, or 5)***")
+                        #display previous orders 
+                        elif (selection == 3):
+                                user.previousOrders(currUserID)
 
 
-#login setup function
-def loginSetup():
+                        #logout of current account
+                        elif (selection == 4):
+                                print("\n --------------- Thanks you for shopping! ---------------\n")
+                                user.loginSetup()
+
+                        #exit the program
+                        elif (selection == 5):
+                                i = 1
+                                print("\n --------------- Thanks you for shopping! ---------------\n")
+
+                                #closing database connection and exiting program
+                                conn.close()
+                                exit()
+
+                        else:
+                                print("\n ***Please enter a valid option. (1, 2, 3, 4, or 5)***")
+
+                except Exception as ex:
+                       print("\n ***Please enter a vaild option. (1, 2, 3, 4, or 5)***")
+
+
+class User:
+    def __init__(self):
+        pass
+ 
+    #login setup function
+    def loginSetup(self):
 
 	#login and validate username and password
-	currUserID = loginValidation()
+                currUserID = self.loginValidation()
 
 	#send user to main store page 
-	mainPage(currUserID)
+                mainPage(currUserID,self)
 	
 
-#login function to check whether the user is in the db or not
-def loginValidation():
+    #login function to check whether the user is in the db or not
+    def loginValidation(self):
 
-	print("\n --------------- Please login to access the shop! ---------------\n")
+                 print("\n --------------- Please login to access the shop! ---------------\n")
 
-	while True:
-	
-		try:
+                 while True:
+        	
+                        try:
 			#username input
-			print("\n ----- Please input your username and password. Enter '0' to exit. -----\n")
-			username = str(input(" Username: "))
+                                print("\n ----- Please input your username and password. Enter '0' to exit. -----\n")
+                                username = str(input(" Username: "))
 
 			#exit the program is user does not want to login
-			if (username == "0"):
-				exit()
+                                if (username == "0"):
+                                        exit()
 
-			password = str(input(" Password: "))
+                                password = str(input(" Password: "))
 
 			#checking to see if username and password is in the database
-			records1 = cur.execute("SELECT USERNAME, USERID FROM USERS WHERE USERNAME = ?", (username,))
-			row1 = records1.fetchone()
+                                records1 = cur.execute("SELECT USERNAME, USERID FROM USERS WHERE USERNAME = ?", (username,))
+                                row1 = records1.fetchone()
 
-			records2 = cur.execute("SELECT PASSWORD FROM USERS WHERE PASSWORD = ?", (password,))
-			row2 = records2.fetchone()
+                                records2 = cur.execute("SELECT PASSWORD FROM USERS WHERE PASSWORD = ?", (password,))
+                                row2 = records2.fetchone()
 
 			#finishing validation process if it is correct
-			if (row1[0] == username and row2[0] == password):
-				userID = row1[1]
-				return userID
-				break
+                                if (row1[0] == username and row2[0] == password):
+                                        userID = row1[1]
+                                        return userID
+                                        break
 
-		except Exception as ex:
-			print("\n ***We do not have a record for the username/password given. Please try again.***")
-
-		
-#displays all the items the store has in stock
-def displayItems():
-
-	print("\n --------------- Current Store Inventory ---------------\n")
-	print("{:<10s}{:<15s}{:<20s}{:<25s}{:<30s}{:<35s}".format("\nItem ID", "Name", "Description","Category","Price", "Quantity"))
-
-	#getting all of the items from inventory
-	records = cur.execute("SELECT * FROM ITEMS")
-
-	#printing out all of the items
-	for row in records:
-		
-		print(" {:<10d}{:<15s}{:<20s}{:<25s}{:<30.2f}{:<35d}".format(row[0], row[1], row[2],row[3],row[4], row[5]))
-
-	return
+                        except Exception as ex:
+                                print("\n ***We do not have a record for the username/password given. Please try again.***")
 
 
-#displays previous orders made by the user
-def previousOrders(currUserID):
 
-	print("\n --------------- Your Previous Orders ---------------\n")
-	orders = True
+    
+        #displays previous orders made by the user
+    def previousOrders(self,currUserID):
+
+        print("\n --------------- Your Previous Orders ---------------\n")
+        orders = True
 	
 	#getting past purchases
-	records = cur.execute("SELECT * FROM PAST_PURCHASES WHERE USERID = ?", (currUserID, ))
-	row = records.fetchone()
+        records = cur.execute("SELECT * FROM PAST_PURCHASES WHERE USERID = ?", (currUserID, ))
+        row = records.fetchone()
 
 	#determining if there are past purchases
-	if (row == None):
-		print("\n ***You have no previous orders associated with this account.***")
-		orders == False
+        if (row == None):
+        	print("\n ***You have no previous orders associated with this account.***")
+        	orders == False
 
-	else:
-		print("{:<10s}{:>15s}{:>20s}{:>25s}{:>30s}".format("\nPurchase ID", "User ID", "Order ID","Total", "Credit Card"))
+        else:
+        	print("{:<10s}{:>15s}{:>20s}{:>25s}{:>30s}".format("\nPurchase ID", "User ID", "Order ID","Total", "Credit Card"))
 
 	#printing out all of the past orders
-	while (orders == True):
-		print("{:<10d}{:>15d}{:>20d}{:>25.2f}{:>30d}".format(row[0], row[1], row[2], row[4], row[3]))
-		row = records.fetchone()
+        while (orders == True):
+        	print("{:<10d}{:>15d}{:>20d}{:>25.2f}{:>30d}".format(row[0], row[1], row[2], row[4], row[3]))
+        	row = records.fetchone()
 
-		if (row == None):
-			break
+        	if (row == None):
+        		break
+
+class Item:
+   def __init__(self):
+           pass
+   #displays all the items the store has in stock
+   def displayItems(self):
+
+        print("\n --------------- Current Store Inventory ---------------\n")
+        print("{:<10s}{:<15s}{:<20s}{:<25s}{:<30s}{:<35s}".format("\nItem ID", "Name", "Description","Category","Price", "Quantity"))
+
+	#getting all of the items from inventory
+        records = cur.execute("SELECT * FROM ITEMS")
+
+	#printing out all of the items
+        for row in records:
+		
+        	print(" {:<10d}{:<15s}{:<20s}{:<25s}{:<30.2f}{:<35d}".format(row[0], row[1], row[2],row[3],row[4], row[5]))
+
+        return
+
 
 #displays items in the current cart
-def cartDisplay(currUserID, shoppingCart):
+class Cart:
+    def __init__(self):
+            pass            
+    def cartDisplay(self,currUserID, shoppingCart):
 
-	if (shoppingCart == []):
-		cartAdd(currUserID)
+        if (shoppingCart == []):
+                self.cartAdd(currUserID)
 
-	print("\n --------------- Your Current Cart ---------------\n")
+        print("\n --------------- Your Current Cart ---------------\n")
 
-	totalPrice = 0
+        totalPrice = 0
 
-	#print out current cart
-	print("{:<10s}{:<15s}{:<20s}{:<25s}{:<30s}".format("\nItem ID", "Name", "Price", "Quantity", "Total Price"))
+        #print out current cart
+        print("{:<10s}{:<15s}{:<20s}{:<25s}{:<30s}".format("\nItem ID", "Name", "Price", "Quantity", "Total Price"))
 
-	for i in range(len(shoppingCart)):
-		currIndex = shoppingCart[i][0]
+        for i in range(len(shoppingCart)):
+                currIndex = shoppingCart[i][0]
 
-		records = cur.execute("SELECT ITEMID, NAME, PRICE, QUANTITY FROM ITEMS WHERE ITEMID = ?", (currIndex,))
-		row = records.fetchone()
+                records = cur.execute("SELECT ITEMID, NAME, PRICE, QUANTITY FROM ITEMS WHERE ITEMID = ?", (currIndex,))
+                row = records.fetchone()
 
-		#find total price of the cart
-		totalPrice = ((row[2] * shoppingCart[i][2]) + totalPrice)
+                #find total price of the cart
+                totalPrice = ((row[2] * shoppingCart[i][2]) + totalPrice)
 
-		print("{:<10d}{:<15s}{:<20.2f}{:<25d}{:<30.2f}".format(row[0], row[1], row[2], shoppingCart[i][2], (row[2] * shoppingCart[i][2])))
+                print("{:<10d}{:<15s}{:<20.2f}{:<25d}{:<30.2f}".format(row[0], row[1], row[2], shoppingCart[i][2], (row[2] * shoppingCart[i][2])))
 
-	#print total of all items in the cart
-	print("{:>75s}".format("------"))
-	print("{:>75.2f}".format(totalPrice))
+        #print total of all items in the cart
+        print("{:>75s}".format("------"))
+        print("{:>75.2f}".format(totalPrice))
 
-	#give the user options on what to do with their cart
-	while True:
-		try:
-			cartStatus = int(input("\n What would you like to do?:\n\n 1. Proceed to checkout\n 2. Remove an item from your cart\n 3. Delete all items in your cart\n Select 1, 2, or 3: "))
+        #give the user options on what to do with their cart
+        while True:
+                #try:
+                        cartStatus = int(input("\n What would you like to do?:\n\n 1. Proceed to checkout\n 2. Remove an item from your cart\n 3. Delete all items in your cart\n Select 1, 2, or 3: "))
 
-			#checkout
-			if (cartStatus == 1):
-				checkout(currUserID, shoppingCart)
-				break
+                        #checkout
+                        if (cartStatus == 1):
+                                checkout(currUserID, shoppingCart)
+                                break
 
-			#remove an item
-			elif (cartStatus == 2):
-				cartRemove(currUserID, shoppingCart)
-				break
+                        #remove an item
+                        elif (cartStatus == 2):
+                                self.cartRemove(currUserID, shoppingCart)
+                                break
 
-			#deletion of cart
-			elif (cartStatus == 3):
-				mainPage(currUserID)
-				break
+                        #deletion of cart
+                        elif (cartStatus == 3):
+                                user=User()
+                                mainPage(currUserID,user)
+                                break
 
-			else:
-				print("\n ***Please input a valid option. (1, 2, or 3)***")
-		
-		except Exception as ex:
-			print("\n ***Please input a valid option. (1, 2, or 3)***")
+                        else:
+                                print("\n ***Please input a valid option. (1, 2, or 3)***")
+                
+                #except Exception as ex:
+                #   print("\n ***Please input a valid option. (1, 2, or 3)***")
 
 	
 #adds items to the cart
-def cartAdd(currUserID):
+    def cartAdd(self,currUserID):
 
-	print("\n --------------- Add Items to your Cart ---------------\n")
+        print("\n --------------- Add Items to your Cart ---------------\n")
 
 	#list for shopping cart
-	shoppingCart = []
+        shoppingCart = []
 
 	#status variavle for loop for adding items to cart
-	shoppingStatus = True
-	while (shoppingStatus == True):
+        shoppingStatus = True
+        while (shoppingStatus == True):
 
 		#getting the item ID and quantity of the desired item
-		itemID = int(input("\n Enter the Item ID # that you would like to add to your cart: "))
-		itemQuantity = int(input("\n Enter the quantity you would like to add to your cart: "))
+        	itemID = int(input("\n Enter the Item ID # that you would like to add to your cart: "))
+        	itemQuantity = int(input("\n Enter the quantity you would like to add to your cart: "))
 
 		#sql to retrieve item info and check if theres that many in stock
-		records = cur.execute("SELECT ITEMID, NAME, PRICE FROM ITEMS WHERE ITEMID = ? AND QUANTITY >= ?", (itemID, itemQuantity,))
-		row = records.fetchone()
+        	records = cur.execute("SELECT ITEMID, NAME, PRICE FROM ITEMS WHERE ITEMID = ? AND QUANTITY >= ?", (itemID, itemQuantity,))
+        	row = records.fetchone()
 
 		#check if the item can be added to cart
-		if (row == None):
-			print ("\n ***The item number that you entered is either not in our system or we do not have enough stock.***")
+        	if (row == None):
+        		print ("\n ***The item number that you entered is either not in our system or we do not have enough stock.***")
 
 		#add item to cart
-		else:
-			shoppingCart.append([row[0], row[1], itemQuantity])
+        	else:
+        		shoppingCart.append([row[0], row[1], itemQuantity])
 
-			print("{:<10s}{:<15s}{:<25s}".format("\nItem ID", "Name", "Price"))
-			print("{:<10d}{:<15s}{:<25.2f}".format(row[0], row[1], row[2]))
+        		print("{:<10s}{:<15s}{:<25s}".format("\nItem ID", "Name", "Price"))
+        		print("{:<10d}{:<15s}{:<25.2f}".format(row[0], row[1], row[2]))
 		
 		#check if the customer wants to do more shopping
-		while True:
-			try:
-				more = int(input("\n Would you like to continue shopping?\n Enter 0 for no\n Enter 1 for yes\n Choice: "))
+        	while True:
+        		try:
+        			more = int(input("\n Would you like to continue shopping?\n Enter 0 for no\n Enter 1 for yes\n Choice: "))
 				
 				#wants to shop more
-				if (more == 1):
-					shoppingStatus = True
-					break
+        			if (more == 1):
+        				shoppingStatus = True
+        				break
 
 				#does not want to shop more
-				elif(more == 0):
-					shoppingStatus = False
-					break
+        			elif(more == 0):
+        				shoppingStatus = False
+        				break
 
-				else:
-					print("\n ***Please input a valid option. (0 or 1)***")
+        			else:
+        				print("\n ***Please input a valid option. (0 or 1)***")
 
-			except Exception as ex:
-				print("\n ***Please input a valid option. (0 or 1)***")
+        		except Exception as ex:
+        			print("\n ***Please input a valid option. (0 or 1)***")
 
 	#display the current cart
-	cartDisplay(currUserID, shoppingCart)
+        self.cartDisplay(currUserID, shoppingCart)
 
 
 #removes items from the cart
-def cartRemove(currUserID, shoppingCart):
+    def cartRemove(self,currUserID, shoppingCart):
 
 	#input to detmine what item and how many should be removed
-	removeId = int(input("\n Please enter the Item ID number that you wish to remove: "))
-	removeQuantity = int(input("\n Please enter the number of this item you want to remove: "))
+        removeId = int(input("\n Please enter the Item ID number that you wish to remove: "))
+        removeQuantity = int(input("\n Please enter the number of this item you want to remove: "))
 
 	#iterating through cart to find item
-	for i in range(len(shoppingCart)):
+        for i in range(len(shoppingCart)):
 		
 		#totally removal of item
-		if ((shoppingCart[i][0] == removeId) and (shoppingCart[i][2] <= removeQuantity)):
-			shoppingCart.pop(i)
-			print("\n This item has been removed from your cart.")
-			break
+        	if ((shoppingCart[i][0] == removeId) and (shoppingCart[i][2] <= removeQuantity)):
+        		shoppingCart.pop(i)
+        		print("\n This item has been removed from your cart.")
+        		break
 
 		#reduction in quantity
-		if ((shoppingCart[i][0] == removeId) and (shoppingCart[i][2] > removeQuantity)):
-			shoppingCart[i][2] = (shoppingCart[i][2] - removeQuantity)
-			print("\n", removeQuantity, "item(s) removed from your cart.")
-			break
+        	if ((shoppingCart[i][0] == removeId) and (shoppingCart[i][2] > removeQuantity)):
+        		shoppingCart[i][2] = (shoppingCart[i][2] - removeQuantity)
+        		print("\n", removeQuantity, "item(s) removed from your cart.")
+        		break
 
 	#displaying cart after changes are made
-	cartDisplay(currUserID, shoppingCart)
+        self.cartDisplay(currUserID, shoppingCart)
 
 
 #checkout when the user is satisfied with their cart
-def checkout(currUserID, shoppingCart):
+def checkout(self,currUserID, shoppingCart):
 
 	#getting address associated with address
 	records = cur.execute("SELECT ADDRESS FROM USERS WHERE USERID = ?", (currUserID, ))
@@ -360,7 +373,8 @@ def checkout(currUserID, shoppingCart):
 	print("{:<10d}{:<15.2f}{:<25d}{:<45s}".format(orderId, total, creditCard, address))
 
 	#returning to main page after purchase is done
-	mainPage(currUserID)
+	user=User()
+	mainPage(currUserID,user)
 
 
 #main function of the program
@@ -369,7 +383,8 @@ def main():
 	print("\n --------------- Welcome to our wonderful store! ---------------\n")
 
 	#function to setup login process
-	loginSetup()
+	user = User()
+	user.loginSetup()
 
 #calling main
 if __name__ == "__main__":
